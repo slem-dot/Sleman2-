17cd5691eb601d254d8a6ae5afb60e5025cc9024
slem-dot <slemanalhassan6@gmail.com>
2026-01-09 21:52:55 +0300

    Update main.py

--- bot/keyboards/main.py
+++ bot/keyboards/main.py
@@ -1,8 +1,17 @@
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 
-def get_main_menu() -> ReplyKeyboardMarkup:
+def user_main_keyboard() -> ReplyKeyboardMarkup:
     keyboard = [
-        ["حساب ايشانسي", "رصيدي"],
-        ["🎁 الإحالات", "📞 التواصل مع الدعم"],
+        [KeyboardButton("💼 حساب ايشانسي"), KeyboardButton("💰 محفظتي")],
+        [KeyboardButton("➕ شحن رصيد البوت"), KeyboardButton("➖ سحب رصيد من البوت")],
+        [KeyboardButton("🆘 دعم")],
+    ]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+def ichancy_keyboard() -> ReplyKeyboardMarkup:
+    keyboard = [
+        [KeyboardButton("1) إنشاء حساب ايشانسي"), KeyboardButton("2) شحن حساب ايشانسي")],
+        [KeyboardButton("3) سحب من حساب ايشانسي")],
+        [KeyboardButton("⬅️ رجوع")],
     ]
     return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
