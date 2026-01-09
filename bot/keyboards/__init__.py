9547b1ab1a53160b2d453b00517dfc69fed22631
slem-dot <slemanalhassan6@gmail.com>
2026-01-08 18:52:38 -0600

    Add files via upload

--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,1 +1,6 @@
+from .main import get_main_menu
+from .balance import get_balance_menu
+from .eish import get_eish_menu
+from .admin import get_admin_menu
 
+__all__ = ["get_main_menu", "get_balance_menu", "get_eish_menu", "get_admin_menu"]
