7fcc1cc2103755603d1a2d9957fcd14230599ddb
slem-dot <slemanalhassan6@gmail.com>
2026-01-08 18:08:18 -0600

    Delete bot/bot directory

--- bot/bot/handlers/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import commands, user, admin, channel, utils
-
-__all__ = ["commands", "user", "admin", "channel", "utils"]
--- bot/bot/services/__init__.py
+++
@@ -1,3 +0,0 @@
-from .database import create_or_update_user, is_user_admin, ensure_admin_user
-
-__all__ = ["create_or_update_user", "is_user_admin", "ensure_admin_user"]
