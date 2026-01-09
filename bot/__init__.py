5fb13c2dad2c989c93a393069e7da2bbcf14c925
slem-dot <slemanalhassan6@gmail.com>
2026-01-09 22:02:12 +0300

    Update and rename __init__.py to constants.py

--- bot/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Ichancy Telegram Bot Package"""
-
-__version__ = '1.0.0'
-__author__ = 'Ichancy Team'
---
+++ bot/constants.py
@@ -0,0 +1,14 @@
+ORDER_TOPUP = "topup"
+ORDER_WITHDRAW = "withdraw"
+ORDER_ICH_CREATE = "ichancy_create"
+ORDER_ICH_TOPUP = "ichancy_topup"
+ORDER_ICH_WITHDRAW = "ichancy_withdraw"
+
+STATUS_PENDING = "pending"
+STATUS_APPROVED = "approved"
+STATUS_REJECTED = "rejected"
+
+CB_CHECK_SUB = "chk_sub"
+CB_ORDER_APPROVE = "ord_ok"
+CB_ORDER_REJECT = "ord_no"
+CB_ORDER_EDIT = "ord_edit"
